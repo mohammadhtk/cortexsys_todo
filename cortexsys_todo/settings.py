dd01a00e50f6556e18c7331d71d35e39527b021a
Mohammad Tavakoli <mohammadh.tavakoli81@gmail.com>
2025-08-26 22:05:16 +0330

    change database to postgres

--- cortexsys_todo/settings.py
+++ cortexsys_todo/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'cortexsys_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cortexsys_todo',
+        'USER': 'mohammad',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
